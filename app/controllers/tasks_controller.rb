ec3550cc74e6978a7dc84cf81926c23188a6bc59
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-05-20 02:17:54 +0000

    finished tasks lab

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,19 @@
 class TasksController < ApplicationController
   def index
     if @current_user
-      @tasks = Task.all
+      @tasks = Task.where({"user_id" => @current_user["id"]})
       @task = Task.new
     end
   end
 
   def create
-    if @current_user
-      @task = Task.new
-      @task["description"] = params["task"]["description"]
-      @task.save
-      redirect_to "/tasks"
-    end
+  
+    @task = Task.new
+    @task["description"] = params["task"]["description"]
+    @task["user_id"] = @current_user["id"]
+    @task.save
+    redirect_to "/tasks"
+    
   end
 
   def destroy
